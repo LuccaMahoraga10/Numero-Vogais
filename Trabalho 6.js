ea2a0120772ce32adcfac74c5da44945e4b1c0a9
Lytergy0 <164952126+LuccaMahoraga10@users.noreply.github.com>
2024-06-05 16:48:04 -0300

    Update Trabalho 6.js
    Funciona ae

--- Trabalho 6.js
+++ Trabalho 6.js
@@ -1,6 +1,6 @@
 function contarVogais(frase) {
     frase = frase.toLowerCase(); // Converter a frase para minúsculas
-    const vogais = 'aeiou';
+    const vogais = 'aeiouáéíóúãõàèìòùäëïöüâêîôû';
     let contador = 0;
     for (let i = 0; i < frase.length; i++) {
         if (vogais.includes(frase[i])) {
@@ -9,3 +9,7 @@ function contarVogais(frase) {
     }
     return contador;
 }
+
+// Exemplo de uso:
+const frase = "Esta é uma frase de exemplo";
+console.log("Número de vogais na frase:", contarVogais(frase));
